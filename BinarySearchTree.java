a239a33f6ce10106be90e04584727ef9ac53fa3a
Sundar Rajan <sundarrajan.srgm@gmail.com>
2019-03-10 22:53:39 +0530

    Bst with insertion tested , print all nodes inorder

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,25 +1,60 @@
 package cuddly_journey;
 
-public class BinarySearchTree<Q> {
+public class BinarySearchTree<Q extends Comparable<Q>> {
 
 	Node<Q> root = null;
 	int no_of_nodes = 0;
 	int height = 0;
 
 	public BinarySearchTree() {
-		root = new Node<Q>();
+		root = null;
 		no_of_nodes = 0;
 		height = 0;
 	}
 
 	public boolean insert(Q val) {
-		Node<Q> new_node = new Node<Q> (val);
-		if (root==null)
-		{
+		Node<Q> new_node = new Node<Q>(val);
+		if (root == null) {
 			root = new_node;
 			return true;
 		}
-		return false;
+
+		Node<Q> cnode = root;
+		Node<Q> parent = null;
+		while (true) {
+			parent = cnode;
+			int compare = val.compareTo(cnode.val());
+			if (compare <= 0) {
+				cnode = cnode.left();
+				if (cnode == null) {
+					parent.setLeft(new_node);
+					return true;
+				}
+			} else if (compare > 0) {
+				cnode = cnode.right();
+				if (cnode == null) {
+					parent.setRight(new_node);
+					return true;
+				}
+			}
+		}
+	}
+
+	public void print() {
+		if (root != null)
+			printAllNodes(root);
+		else
+			System.out.println("Warning - Empty Tree");
+	}
+
+	private void printAllNodes(Node<Q> node) {
+		System.out.printf("Node : %s, Left : %s, Right : %s\n", node.val(),
+				node.left() == null ? "null" : node.left().val(), node.right() == null ? "null" : node.right().val());
+		if (node.left() != null)
+			printAllNodes(node.left());
+		if (node.right() != null)
+			printAllNodes(node.right());
+
 	}
 
 	public void find(Q val) {
@@ -30,16 +65,11 @@ public class BinarySearchTree<Q> {
 
 	}
 
-	// Unit test purpose
-	public static void main(String[] args) {
-
-	}
-
 	class Node<T> {
 		T val;
-		Node<T> left;
-		Node<T> right;
-		Node<T> parent;
+		private Node<T> left;
+		private Node<T> right;
+		private Node<T> parent;
 
 		public Node() {
 			val = null;
@@ -47,7 +77,7 @@ public class BinarySearchTree<Q> {
 			right = null;
 			parent = null;
 		}
-		
+
 		public Node(T val) {
 			this();
 			this.val = val;
@@ -84,6 +114,17 @@ public class BinarySearchTree<Q> {
 		public T val() {
 			return val;
 		}
+	}
+
+	// Unit test purpose
+	public static void main(String[] args) {
+		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+		bst.insert(5);
+		bst.insert(25);
+		bst.insert(15);
+		bst.insert(10);
+		bst.insert(35);
+		bst.print();
 	}
 
 }
