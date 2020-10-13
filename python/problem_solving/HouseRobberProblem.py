608d90e9aa5b34c3e59a00feabdfc8ed03fa4a10
Sundar Rajan <sundarrajan.r@amadeus.com>
2020-10-13 21:57:07 +0530

    Fixed all test cases of hr rank and learnt how optimal solution in local can evolve to global optimum.

--- python/problem_solving/HouseRobberProblem.py
+++ python/problem_solving/HouseRobberProblem.py
@@ -1,6 +1,8 @@
 # https://www.hackerrank.com/challenges/max-array-sum/problem
-# Same as house robber problem where he cannot rob consecutive houses, but with huge no of negative sum many cases fails.
-# Complete the maxSubsetSum function below.
+# Same as house robber problem but negative values present in array makes it even more interesting.
+# choose maximum of arr[n] + f(n-2), f(n-1), arr[n]
+# it differs from house robber problem by the fact it includes negative cost
+# also select current array index if prev choice includes a neg cost.
 def maxSubsetSum(nums):
     n = len(nums)
     if n == 0:
@@ -12,12 +14,12 @@ def maxSubsetSum(nums):
     a = nums[0]
     b = max(a, nums[1])
     for i in range(2, len(nums)):
-        b, a = max(nums[i],nums[i] + a, b), b
+        b, a = max(nums[i], nums[i] + a, b), b
     return b
 
 
 def main():
-    res = maxSubsetSum([-2, -1, -3, -1, 1, 0, -2, 3, 1, 0, -1, 1, -1])
+    res = maxSubsetSum([-2, 1, 3, -4, 5])
     print(res)
 
 
